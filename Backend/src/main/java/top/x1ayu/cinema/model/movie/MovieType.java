0eee1e97fbe3f50256a28919d0e61b7e2bb13b95
lizey <13377540435@163.com>
2025-03-20 17:52:00 +0800

    修正

---
+++ Backend/src/main/java/top/x1ayu/cinema/model/movie/MovieType.java
@@ -0,0 +1,13 @@
+package top.x1ayu.cinema.model.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieType {
+    private Integer id;
+    private Integer name;
+}
---
+++ Backend/src/main/java/top/x1ayu/cinema/model/order/Order.java
@@ -0,0 +1,36 @@
+package top.x1ayu.cinema.model.order;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Order {
+    private Long id;
+    private Long movie_id;
+    private Long payer_id;
+    private LocalDateTime order_time;
+    private BigDecimal total_price;
+    private Integer ticket_count;
+    private OrderStatus status;
+    private PlayerType player_type;
+
+    private List<Ticket> tickets;
+
+
+    public enum OrderStatus {
+        NOT_PAID,
+        PAID,
+        CHANGE_TICKET,
+        CANCELED,
+        REFUNDED,
+        CHANGED,
+        CANCELLED
+    }
+
+    public enum PlayerType {
+        CASH,
+        ALIPAY,
+        WECHAT,
+        CREDIT_CARD
+    }
+}
